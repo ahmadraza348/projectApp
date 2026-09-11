cf68508535dfbb96d42b940d37f3ba822020bff2
Ahmad Raza <engr.ahmadraza348@gmail.com>
2026-09-11 11:53:46 +0500

    empty

--- database/migrations/2026_09_11_000000_create_departments_table.php
+++ database/migrations/2026_09_11_000000_create_departments_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->boolean('status')->default(true);
             $table->softDeletes();
             $table->timestamps();
-            $table->softDelete();
         });
     }
 
